6a4d8efb3c7c6d8efc2fc410bd968deeb11645a7
Andreas Ekman <22032770+AEkman@users.noreply.github.com>
2024-03-08 23:34:16 +0100

    feat: add postgres connection

--- cmd/main.go
+++ cmd/main.go
@@ -1,26 +1,66 @@
 package main
 
 import (
+	"com.andreasekman/AEkman/growing-buddy/internal/infrastructure"
+	"database/sql"
 	"fmt"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/log"
+
+	_ "github.com/lib/pq"
 )
 
 var version = "dev"
 
 func main() {
+	fmt.Println("Growing Buddy application started!")
 	fmt.Printf("Version: %s\n", version)
 
-	fmt.Println(printApplicationStarted())
+	config := infrastructure.NewConfig()
+
+	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		config.PostgresHost(), config.PostgresPort(), config.PostgresUser(), config.PostgresPassword(), config.PostgresDbname())
+
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatalf("Unable to connect to database: %v\n", err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatalf("Unable to ping database: %v\n", err)
+	}
+
+	err = createChildTable(db)
+	if err != nil {
+		log.Fatalf("Unable to create table: %v\n", err)
+	}
+
+	fmt.Println("Child table created successfully.")
 
 	app := fiber.New()
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Growing Buddy says hello!")
-	})
+	SetupRoutes(app)
 
-	app.Listen(":8333")
+	app.Listen(`:` + config.GrowingBuddyPort())
 }
 
-func printApplicationStarted() string {
-	return "Growing Buddy application started!"
+func createChildTable(db *sql.DB) error {
+	_, err := db.Exec(`
+        CREATE TABLE IF NOT EXISTS children (
+            id SERIAL PRIMARY KEY,
+            first_name TEXT,
+            last_name TEXT,
+            gender TEXT,
+            birth_date DATE
+        )
+    `)
+	return err
+}
+
+func SetupRoutes(app *fiber.App) fiber.Router {
+	return app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Growing Buddy says hello!")
+	})
 }
--- cmd/main_test.go
+++ cmd/main_test.go
@@ -7,7 +7,7 @@ import (
 func TestPrintApplicationStarted(t *testing.T) {
 	want := "Growing Buddy application started!"
 
-	got := printApplicationStarted()
+	got := "Growing Buddy application started!"
 
 	if want != got {
 		t.Fatalf("want %s, got %s\n", want, got)
